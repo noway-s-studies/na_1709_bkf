88939dfc7d97dd6a667fb32e3623db9a1386edbf
noway0032@gmail.com <noway0032@gmail.com>
2018-09-05 20:03:48 +0200

    part2end

--- ShoppingKotlin/app/src/main/java/eu/artouch/shoppingkotlin/MainActivity.kt
+++ ShoppingKotlin/app/src/main/java/eu/artouch/shoppingkotlin/MainActivity.kt
@@ -1,39 +1,57 @@
 package eu.artouch.shoppingkotlin
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter = ShoppingAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        rvShoppingItems.adapter = adapter
+
+        btnAddItem.setOnClickListener {
+            val name = etName.text.toString()
+            val price = etPrice.text.toString()
+
+            if(name.isNotBlank() && price.isNotBlank()) {
+                createItem(name, price)
+            }
         }
     }
 
+    private fun createItem(name: String, price: String) {
+        val newItem = ShoppingItem(name, price.toInt())
+
+        adapter.addItem(newItem)
+
+        etName.setText("")
+        etPrice.setText("")
+
+        rvShoppingItems.smoothScrollToPosition(adapter.itemCount)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
+        return when(item.itemId) {
+            R.id.action_delete_all->{
+                adapter.removeAllItems()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
---
+++ ShoppingKotlin/app/src/main/java/eu/artouch/shoppingkotlin/ShoppingItem.kt
@@ -0,0 +1,3 @@
+package eu.artouch.shoppingkotlin
+
+data class ShoppingItem(val name: String, val price: Int)
